12a7efbdb210362b7a431e8d75770f25be44da72
Jefferson Maldonado <jeffmaldo27@gmail.com>
2019-07-06 22:04:05 -0500

    added site structure

--- application.py
+++ application.py
@@ -1,11 +1,12 @@
-import os
-
-from flask import Flask, session
+import os, requests
+import pprint as pp
+from flask import Flask, session, render_template
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
+KEY = "xU4eCPbV2hTYGBIO0qhnQ" #goodreads api key
 
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
@@ -23,4 +24,16 @@ db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
 def index():
+    res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": KEY, "isbns": "9781632168146"})
+    pp.pprint(res.json())
     return "Project 1: TODO"
+
+@app.route("/test/")
+def test():
+    #flights = db.execute("SELECT * FROM flights").fetchall()
+    return render_template("login.html")
+
+@app.route("/book/")
+def book():
+    #flights = db.execute("SELECT * FROM flights").fetchall()
+    return render_template("book.html")
---
+++ import.py
@@ -0,0 +1,1 @@
+#TODO: write CSV TO SQL import script 
