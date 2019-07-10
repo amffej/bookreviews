2b90fcabae7abdbb6db210edb1fd431c4ed64b19
Jefferson Maldonado <jeffmaldo27@gmail.com>
2019-07-10 02:29:58 -0500

    Pre-submission

--- README.md
+++ README.md
@@ -1,3 +1,93 @@
 # Project 1
 
-Web Programming with Python and JavaScript
+### Project Summary:
+The goal for this project was to build a book review website. Requirements were as follow:
+Users will be able to register for your website and then log in using their username and password. Once they log in, they will be able to search for books, leave reviews for individual books, and see the reviews made by other people. You’ll also use the a third-party API by Goodreads, another book review website, to pull in ratings from a broader audience. Finally, users will be able to query for book details and book reviews programmatically via your website’s API.
+
+
+### File Structure:
+```
+project1-jeffmaldo27/
+├── templates/      
+│   ├── book.html       # Displays individual books and reviews
+│   ├── index.html      # Homepage for search bar and results
+│   ├── layout.html     # Shared layout file
+│   ├── login.html      # Login form
+│   └── register.html   # Registration form
+├── static/
+│   ├── gr-logo.png     # gooreads logo
+│   └── style.css       # Custom CSS file
+├── application.py      # Main flask application file
+├── books.csv           # CSV used for importing book data
+├── import.py           # Script used for import CSV to SQL data
+├── README.md           # This file!
+└── requirements.txt    # List required packages
+```
+
+### Database Structure:
+For this project we used heroku app services to host a PostgreSQL.
+
+SQL Structure is as follow:
+
+```
+-- Adminer 4.6.3-dev PostgreSQL dump
+
+DROP TABLE IF EXISTS "books";
+DROP SEQUENCE IF EXISTS books_id_seq;
+CREATE SEQUENCE books_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 2147483647 START 1 CACHE 1;
+
+CREATE TABLE "public"."books" (
+    "id" integer DEFAULT nextval('books_id_seq') NOT NULL,
+    "isbn" character varying NOT NULL,
+    "title" character varying NOT NULL,
+    "author" character varying NOT NULL,
+    "year" integer NOT NULL,
+    CONSTRAINT "books_id" PRIMARY KEY ("id")
+) WITH (oids = false);
+
+
+DROP TABLE IF EXISTS "reviews";
+DROP SEQUENCE IF EXISTS reviews_id_seq;
+CREATE SEQUENCE reviews_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 2147483647 START 1 CACHE 1;
+
+CREATE TABLE "public"."reviews" (
+    "id" integer DEFAULT nextval('reviews_id_seq') NOT NULL,
+    "book_id" integer NOT NULL,
+    "user_id" integer NOT NULL,
+    "review_text" text NOT NULL,
+    CONSTRAINT "reviews_book_id_fkey" FOREIGN KEY (book_id) REFERENCES books(id) NOT DEFERRABLE,
+    CONSTRAINT "reviews_user_id_fkey" FOREIGN KEY (user_id) REFERENCES users(id) NOT DEFERRABLE
+) WITH (oids = false);
+
+
+DROP TABLE IF EXISTS "users";
+DROP SEQUENCE IF EXISTS user_id_seq;
+CREATE SEQUENCE user_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 2147483647 START 1 CACHE 1;
+
+CREATE TABLE "public"."users" (
+    "id" integer DEFAULT nextval('user_id_seq') NOT NULL,
+    "firstname" character varying NOT NULL,
+    "lastname" character varying NOT NULL,
+    "username" character varying NOT NULL,
+    "password" character varying NOT NULL,
+    "email" character varying NOT NULL,
+    CONSTRAINT "user_id" PRIMARY KEY ("id")
+) WITH (oids = false);
+
+-- 2019-07-8 06:27:32.899362+00
+```
+
+### Other Features:
+
+- Search for capitalized entries in database
+- Genera database safe password using md5 + salt
+- Dynamic menu fields based on user sign in/out status
+- Bootstrap cards for Desktop and Mobile friendly viewing
+
+### Future improvements:
+
+- When search result is 4 or less items, the cards dont alight in most optimal manner.
+- Add more details on the review, including time stamp.
+- Add a 5 star CSS book review button.
+
+
--- application.py
+++ application.py
@@ -1,4 +1,7 @@
-import os, json, hashlib
+import os
+import json
+import hashlib
+import requests
 import pprint as pp
 from flask import Flask, session, render_template, request, redirect, url_for
 from flask_session import Session
@@ -6,7 +9,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
-KEY = "xU4eCPbV2hTYGBIO0qhnQ" #goodreads api key
+
+# goodreads api key
+KEY = "xU4eCPbV2hTYGBIO0qhnQ"  
 
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
@@ -21,28 +26,74 @@ Session(app)
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
-
+# Home page, process search results
 @app.route("/", methods=["GET"])
 def index():
+    # Check is user is logged in
     if 'username' in session:
-        firstname= session['firstname']
+        firstname = session['firstname']
         firstname = firstname.capitalize()
         search_data = request.args.get("search")
         search_results = []
+        # Process search queries
         if search_data is not None:
             search_data_formated = '%{0}%'.format(search_data)
             search_data_capitalized = search_data.capitalize()
-            search_data_capitalized_formated = '%{0}%'.format(search_data_capitalized)
+            search_data_capitalized_formated = '%{0}%'.format(
+                search_data_capitalized)
             pp.pprint(f"Searching for: {search_data_capitalized_formated}")
-            search_results = db.execute("select * from books where title LIKE :search or title LIKE :search_cap or isbn LIKE :search or author LIKE :search or author LIKE :search_cap", {"search" : search_data_formated, "search_cap" : search_data_capitalized_formated}).fetchall()
-            #search_results = db.execute("select title from books where title LIKE :search or isbn LIKE :search or author LIKE :search", {"search" : search_data_formated})
-            #pp.pprint(search_results)
+            search_results = db.execute("select * from books where title LIKE :search or title LIKE :search_cap or isbn LIKE :search or author LIKE :search or author LIKE :search_cap", {
+                                        "search": search_data_formated, "search_cap": search_data_capitalized_formated}).fetchall()
             for book in search_results:
                 pp.pprint(book.title)
-        return render_template("index.html", signed_in=True, firstname=firstname, results = search_results)
+        return render_template("index.html", signed_in=True, firstname=firstname, results=search_results, search_data = search_data)
     return redirect(url_for('login'))
-    
 
+# Book Page, Process all book details, Goodread API, Determine user reviewd book
+@app.route("/book/", methods=["GET"])
+def book():
+    # Check if user is logged in
+    if 'username' in session:
+        firstname = session['firstname']
+        firstname = firstname.capitalize()
+        user = session['user_id']
+        search_data = request.args.get("id")
+        # Pull book information from database
+        if search_data is not None:
+            book_details = db.execute("select * from books where id = :id", {"id": search_data}).fetchall()
+            # Pull goodreads data for current book
+            for book in book_details:
+                goodread_raw = requests.get(
+                    "https://www.goodreads.com/book/review_counts.json", params={"key": KEY, "isbns": book.isbn})
+                goodreads_json = goodread_raw.json()
+                for data in goodreads_json['books']:
+                    goodreads_rating = data['average_rating']
+                    goodreads_count = data['ratings_count']
+            # Get all reviews for current book
+            reviews = db.execute("select review_text, username FROM reviews LEFT JOIN users ON reviews.user_id=users.id WHERE book_id = :book_id ", {
+                                 "book_id": search_data}).fetchall()
+            # Find out if user already reviewd this book
+            if db.execute("SELECT * FROM reviews WHERE book_id = :book_id AND user_id = :user_id", {"book_id": search_data, "user_id": user}).rowcount > 0:
+                user_reviewed = True
+            else:
+                user_reviewed = False
+        return render_template("book.html", signed_in=True, firstname=firstname, results=book_details, goodreads_rating=goodreads_rating, goodreads_count=goodreads_count, reviews=reviews, user_reviewed=user_reviewed)
+    return redirect(url_for('login'))
+
+# Inputs user review into database and returns user to page
+@app.route("/review", methods=["POST"])
+def review():
+    if request.method == "POST":
+        review = request.form.get("review_txt")
+        book = request.form.get("book_id")
+        user = session['user_id']
+        db.execute("INSERT INTO reviews (book_id, user_id, review_text) VALUES (:book, :user, :review)",
+                   {"book": book, "user": user, "review": review})
+        db.commit()
+        prev_url = url_for('book') + "?id=" + book
+    return redirect(prev_url)
+
+# Allows user to register to the site
 @app.route("/register", methods=["POST", "GET"])
 def register():
     if request.method == "POST":
@@ -52,7 +103,7 @@ def register():
         uname = request.form.get("username")
         email = request.form.get("email")
         password = request.form.get("password")
-                # Make password database safe
+        # Make password database safe
         salt = "4xO9"
         db_password = password+salt
         password_hash = hashlib.md5(db_password.encode())
@@ -60,13 +111,14 @@ def register():
 
         # Check that Username is not taken
         if db.execute("SELECT * FROM users WHERE username = :username", {"username": uname}).rowcount > 0:
-            return render_template("register.html", alert=True) 
+            return render_template("register.html", alert=True)
         db.execute("INSERT INTO users (firstname, lastname, username, password, email) VALUES (:fname, :lname, :uname, :password, :email )",
-                {"fname": fname, "lname": lname, "uname" : uname, "password" : password_hash.hexdigest(), "email" : email})
+                   {"fname": fname, "lname": lname, "uname": uname, "password": password_hash.hexdigest(), "email": email})
         db.commit()
         return render_template("register.html", success_message=True)
     return render_template("register.html")
 
+# Allows user to login to the site
 @app.route("/login", methods=["POST", "GET"])
 def login():
     if request.method == "POST":
@@ -77,52 +129,52 @@ def login():
         # Check if user is in database
         if db.execute("SELECT * FROM users WHERE username = :username", {"username": uname}).rowcount == 0:
             return render_template("login.html", alert=True)
-        
+
         # Get user information from database
-        db_userinfo = db.execute("SELECT * FROM users where username = :username", {"username": uname}).fetchone()
-        
+        db_userinfo = db.execute(
+            "SELECT * FROM users where username = :username", {"username": uname}).fetchone()
+
         # Compare input password vs stored password
         salt = "4xO9"
         db_password = password+salt
         password_hash = hashlib.md5(db_password.encode())
         password_check = (db_userinfo.password == password_hash.hexdigest())
 
+        # If password matches start the sessiond
         if password_check:
             session['username'] = db_userinfo.username
             session['firstname'] = db_userinfo.firstname
             session['lastname'] = db_userinfo.lastname
+            session['user_id'] = db_userinfo.id
             return redirect(url_for('index'))
-        return render_template("login.html", alert=True) 
+        return render_template("login.html", alert=True)
     return render_template("login.html")
 
+# Logs user out of site
 @app.route('/logout')
 def logout():
-   # remove the username from the session if it is there
-   session.pop('username', None)
-   return redirect(url_for('index'))
+    # remove the username from the session if it is there
+    session.pop('username', None)
+    return redirect(url_for('index'))
 
-@app.route("/book")
-def book():
-    #res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": KEY, "isbns": "0380795272"})
-    #pp.pprint(res.json())
-    #flights = db.execute("SELECT * FROM flights").fetchall()
-    return render_template("book.html")
-
+# External API for site information
 @app.route("/api/<string:isbn>")
 def api(isbn):
-   
+
     # Checking if book is in database
     if db.execute("SELECT * FROM books WHERE isbn = :isbn", {"isbn": isbn}).rowcount == 0:
-       return json.dumps({'error': 'Not found'}), 404
+        return json.dumps({'error': 'Not found'}), 404
 
     # Get book info from database (books)
-    book_data = db.execute("SELECT * FROM books WHERE isbn = :isbn", {"isbn": isbn}).fetchall()
-    
+    book_data = db.execute(
+        "SELECT * FROM books WHERE isbn = :isbn", {"isbn": isbn}).fetchall()
+
     # Get number of reviews from database (reviews) and setting json output
     for book in book_data:
-        total_Reviews = db.execute("SELECT COUNT(id) FROM reviews where book_id = :id", {"id": book.id}).fetchone()
-        json_output = json.dumps({'title': book.title, 'author': book.author, 'year' : book.year, 'isbn': book.isbn, 'review_count': total_Reviews.count })
-    
+        total_Reviews = db.execute("SELECT COUNT(id) FROM reviews where book_id = :id", {
+                                   "id": book.id}).fetchone()
+        json_output = json.dumps({'title': book.title, 'author': book.author,
+                                  'year': book.year, 'isbn': book.isbn, 'review_count': total_Reviews.count})
+
     db.commit()
     return json_output
-
